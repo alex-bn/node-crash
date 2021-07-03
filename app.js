121897aa42fd4b40a02e6d2442a19f145ef95b24
Ion Bajan <ion.bajan@uti.ro>
2021-07-03 10:05:55 +0300

    Second commit

--- app.js
+++ app.js
@@ -8,3 +8,7 @@ setTimeout(() => {
 }, 0);
 console.log(3, 'Third task');
 console.log(4, 'Fourth task');
+
+
+// Event loop - > is what allows Nodejs to perform non-blocking I/O operations - despite the fact that JS is single threaded - by offloading operations to the system kernel whenever possible.
+
